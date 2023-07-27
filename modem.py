90b290c5ee6062289fe175090f55e325b3e7d5ee
Vincnet F. Manzo <vinatron@localhost.localdomain>
2023-07-27 11:15:50 -0400

    modified:   modem.py

--- modem.py
+++ modem.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import socket
 import time
 from contextlib import suppress
@@ -5,19 +6,20 @@ from contextlib import suppress
 def do_nothing():
     pass
 
-PDOS = '192.168.0.79'
-ICC = '192.168.0.76'
-PORT = 3276
+PDOS = '10.0.0.1'
+ICC = '10.0.0.2'
+CPORT = 3270
+SPORT = 3270
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((PDOS, PORT))
+server.bind((PDOS, CPORT))
 
 
 server.listen(0)
 pdos_socket, address = server.accept()
 print(f"Connected to {address}")
-client.connect((ICC, PORT))
+client.connect((ICC, SPORT))
 
 #time.sleep(1)
 #client.send(b'\xff\xfc\x28')
@@ -26,7 +28,6 @@ idata = client.recv(1024)
 print(f'Data from ICC is: {idata}')
 idatlen = len(idata)
     #print(f'{idatlen}')
-    
 print(f'Data Length from ICC is {idatlen}')
 pdos_socket.send(idata)
 while True:
@@ -36,7 +37,7 @@ while True:
     #print(f'{pdatlen}')
     rxbuff = bytearray(b'')
     running = True
-    msec = 100000000
+    msec = 2
     end = 0
     while running:
         print(msec)
@@ -46,6 +47,7 @@ while True:
         try:
             data = pdos_socket.recv(1)
         except:
+            #data = b''
             print("got exception")
         print("after")
         lendata = len(data)
@@ -61,7 +63,6 @@ while True:
         if msec <= end:
             running = False
             print(f'RX Buffer Full Data: {rxbuff}')
-          
         pdata = rxbuff
         print(f"Data from PDOS is: {pdata}")
         pdatlen = len(pdata)
